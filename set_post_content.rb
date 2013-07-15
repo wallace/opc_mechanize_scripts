63c0227164be42bd97e676e5cb5845ed00539b9f
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2013-07-14 21:09:56 -0400

    Updates script for new version of podbean

--- set_post_content.rb
+++ set_post_content.rb
@@ -25,7 +25,25 @@ login_form.pwd = ENV['OPCUSA_PASSWORD']
 login_form.submit
 
 agent.page.link_with(:text => "Publish").click
-agent.page.link_with(:text => day_link).click
-post_form = agent.page.form_with(:name => "post")
+
+tbody = agent.page.at("//table//tbody")
+
+post_to_edit = nil
+tbody.children.each do |row|
+  row.children.each do |column|
+    if column && column.children && column.children.first &&
+      column.children.first.content =~ /#{day}/
+      post_to_edit = row
+    end
+  end
+end
+
+# open up the edit page for the post
+agent.get(post_to_edit.css("a[@href*='edit']").first.attributes["href"])
+
+post_form = agent.page.form_with(:action => "/admin/post.php")
 post_form.content = "<p><em>#{sermon_title}</em></p><p>#{preacher}</p>"
+
+# Change to draft and submit
+post_form.buttons[0].value = "Save draft"
 post_form.submit
